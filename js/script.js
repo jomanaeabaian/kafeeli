ba274ecac5a99fbda47b7f63199aaf6b9432dc13
Jomana <jomanaeabaian@gmail.com>
2026-07-06 18:39:31 +0300

    editing responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector(".menu-open");
 const menu = document.querySelector(".mobile-menu");
 const menu_list = document.querySelector(".mobile-menu ul");
+const close_btn = document.querySelector(".close-btn");
+
 
 btn.onclick = () => {
     menu.classList.remove("opacity-0", "invisible");
@@ -9,6 +11,11 @@ btn.onclick = () => {
 
 
 menu.onclick = () => {
+    menu.classList.add("opacity-0", "invisible");
+    menu_list.classList.add("-translate-x-full");
+};
+
+close_btn.onclick = () => {
     menu.classList.add("opacity-0", "invisible");
     menu_list.classList.add("-translate-x-full");
 };
